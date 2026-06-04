f0d24cde3a4d9cfbaec7d1fc6b343758606864d7
gudangwarnapji-byte <gudangwarnapji@gmail.com>
2026-06-04 14:12:18 +0700

    Update useHist.js

--- src/composables/useHist.js
+++ src/composables/useHist.js
@@ -4,8 +4,13 @@ export const activeHistId = ref('')
 
 export function useHist() {
   const bukaRiwayat = (id) => {
-    console.log('bukaRiwayat dipanggil:', id)  // ← tambah ini
+    console.log('bukaRiwayat dipanggil:', id)
     activeHistId.value = id
   }
+
+  const tutupRiwayat = () => {
+    activeHistId.value = ''
+  }
+
   return { bukaRiwayat, tutupRiwayat }
 }
