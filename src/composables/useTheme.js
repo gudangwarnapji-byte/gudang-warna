5f20322738e2faa02c9f277ec423cd6e0e57b947
Masfiyal Illah <gudangwarnapji@gmail.com>
2026-06-14 12:32:58 +0700

    Update useTheme.js

--- src/composables/useTheme.js
+++ src/composables/useTheme.js
@@ -7,13 +7,15 @@ const toggleTheme = () => {
   isDarkMode.value = !isDarkMode.value
 }
 
-// Bikin efek "Bunglon": kalau berubah, ganti atribut HTML dan simpan ke memori
+// Bikin efek "Bunglon" untuk HTML dan Bootstrap 5.3
 watch(isDarkMode, (newVal) => {
   if (newVal) {
     document.documentElement.setAttribute('data-theme', 'dark')
+    document.documentElement.setAttribute('data-bs-theme', 'dark') // Aktifkan dark mode Bootstrap 5.3
     localStorage.setItem('theme', 'dark')
   } else {
     document.documentElement.setAttribute('data-theme', 'light')
+    document.documentElement.setAttribute('data-bs-theme', 'light') // Aktifkan light mode Bootstrap 5.3
     localStorage.setItem('theme', 'light')
   }
 }, { immediate: true })
